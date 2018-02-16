7acc47553b9ccddf044063f5781910069c33856d
morskibg <dimityrp@yahoo.com>
2018-02-16 23:09:12 +0200

    +

--- Exercise2/P03_JediGalaxy/Program.cs
+++ Exercise2/P03_JediGalaxy/Program.cs
@@ -3,34 +3,58 @@ using System.Linq;
 
 namespace P03_JediGalaxy
 {
+    
     class Program
     {
-        static void Main()
+        static int[] InputToIntArr(Func<string> Input)
         {
-            int[] dimestions = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-            int x = dimestions[0];
-            int y = dimestions[1];
+            int[] arr = Input().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            return arr;
+        }
+        static int[] InputStringToIntArr(string Input)
+        {
+            int[] arr = Input.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            return arr;
+        }
 
-            int[,] matrix = new int[x, y];
-
-            int value = 0;
+        static void FillMatrix(int[,] matrix)
+        {
+            int x = matrix.GetLength(0);
+            int y = matrix.GetLength(1);
             for (int i = 0; i < x; i++)
             {
                 for (int j = 0; j < y; j++)
                 {
-                    matrix[i, j] = value++;
+                    matrix[i, j] = y * i + j;
                 }
             }
+        }
 
-            string command = Console.ReadLine();
+        
+        static void Main()
+        {
+            
+            int[] dimestions = InputToIntArr(Console.ReadLine);
+            int rows = dimestions[0];
+            int cols = dimestions[1];
+            int[,] matrix = new int[rows, cols];
+            FillMatrix(matrix);
+            
+
+            
             long sum = 0;
-            while (command != "Let the Force be with you")
+            while (true)
             {
-                int[] ivoS = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-                int[] evil = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                string command = Console.ReadLine();
+                if (command == "Let the Force be with you")
+                {
+                    break;
+                }
+                int[] ivoS = InputStringToIntArr(command);
+                int[] evil = InputToIntArr(Console.ReadLine);
                 int xE = evil[0];
                 int yE = evil[1];
-
+               
                 while (xE >= 0 && yE >= 0)
                 {
                     if (xE >= 0 && xE < matrix.GetLength(0) && yE >= 0 && yE < matrix.GetLength(1))
@@ -43,6 +67,7 @@ namespace P03_JediGalaxy
 
                 int xI = ivoS[0];
                 int yI = ivoS[1];
+              
 
                 while (xI >= 0 && yI < matrix.GetLength(1))
                 {
@@ -55,7 +80,7 @@ namespace P03_JediGalaxy
                     xI--;
                 }
 
-                command = Console.ReadLine();
+
             }
 
             Console.WriteLine(sum);
