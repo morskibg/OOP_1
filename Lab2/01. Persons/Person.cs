0c9f0bd6a255a0c3c954b8593d7b76558279dc52
morskibg <dimityrp@yahoo.com>
2018-02-19 21:41:28 +0200

    iii

---
+++ Lab2/01. Persons/Person.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Person
+{
+    private string firstName;
+    private string lastName;
+    private int age;
+    private decimal salary;
+
+    public string FirstName
+    {
+        get { return firstName; }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+            }
+            lastName = value;
+        }
+    }
+
+    public string LastName
+    {
+        get { return lastName; }
+        private set
+        {
+            if (value.Length < 3)
+            {
+                throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+            }
+            lastName = value;
+        }
+    }
+
+    public int Age
+    {
+        get { return age; }
+        private set
+        {
+            if (value <=0 )
+            {
+                throw new ArgumentException("Age cannot be zero or a negative integer!");
+            }
+            age = value;
+        }
+    }
+
+    public decimal Salary
+    {
+        get { return salary; }
+        private set
+        {
+            if (value < 460)
+            {
+                throw new ArgumentException("Salary cannot be less than 460 leva!");
+            }
+        }
+    }
+   
+    public Person(string fName, string lName, int pAge)
+    {
+        FirstName = fName;
+        LastName = lName;
+        Age = pAge;
+    }
+
+    public Person(string fName, string lName, int pAge, decimal sal):this(fName, lName, pAge)
+    {
+        Salary = sal;
+    }
+
+    public void IncreaseSalary(decimal bonus)
+    {
+        decimal increase = salary * (bonus / 100);
+        if (age < 30)
+        {
+            salary += increase / 2;
+        }
+        else
+        {
+            salary += increase;
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName} receives {salary:f2} leva.";
+    }
+}
+
---
+++ Lab2/01. Persons/Team.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+public class Team
+{
+    private string name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+    public List<Person> FirstTeam {
+        get { return this.firstTeam; }
+    }
+
+    public List<Person> ReserveTeam
+    {
+        get { return this.reserveTeam; }
+        
+    }
+
+    public Team(string name)
+    {
+        this.name = name;
+        this.firstTeam = new List<Person>();
+        this.reserveTeam = new List<Person>();
+        
+    }
+
+    public void AddPlayer(Person person)
+    {
+        if (person.Age < 40)
+        {
+            FirstTeam.Add(person);
+        }
+        else
+        {
+            ReserveTeam.Add(person);
+        }
+    }
+
+    public override string ToString()
+    {
+        return $"First team has {this.FirstTeam.Count} players. \nReserve team has {this.ReserveTeam.Count} players.";
+    }
+}
+
